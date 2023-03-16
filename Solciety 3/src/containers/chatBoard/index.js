4e46b35a087581fb8833c18145f9aea631315bd7
nosselil53 <115351778+nosselil53@users.noreply.github.com>
2023-03-16 22:19:23 +0100

    Add files via upload

---
+++ Solciety 3/src/containers/chatBoard/index.js
@@ -0,0 +1,11 @@
+import Chat from './chat'
+
+const ChitChat = () => {
+    return (
+        <div className="chitchat-main " id="content">
+            <Chat />
+        </div>
+    );
+}
+
+export default ChitChat;
